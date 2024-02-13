715df3e3481fe4a5b269eb2d97da4ada7cfd1de1
llsnpy <pashka271196@gmail.com>
2024-02-13 10:39:38 +0300

    initial commit

---
+++ src/main/java/by/mironenko/orderingWithCamuda/OrderingWithCamudaApplication.java
@@ -0,0 +1,13 @@
+package by.mironenko.orderingWithCamuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingWithCamudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingWithCamudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/mironenko/orderingWithCamuda/OrderingWithCamudaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mironenko.orderingWithCamuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingWithCamudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
